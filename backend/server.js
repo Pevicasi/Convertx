5890b8d70ef3bafc51a19526ed3fcf72068ffcc1
Pevicasi <130881491+Pevicasi@users.noreply.github.com>
2026-06-26 20:38:00 -0300

    Delete backend directory

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const { port } = require('./config/appConfig');
-const { startCleanupJob } = require('./services/cleanupService');
-
-app.listen(port, () => {
-  console.log(`ConvertX rodando na porta ${port}`);
-  startCleanupJob();
-});
